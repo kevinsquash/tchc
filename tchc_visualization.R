58f6de1abb8f218f6e5dfd6ab574a8e994f0f41e
kevinsquash <kevinsquash@gmail.com>
2016-03-14 16:47:43 -0400

    Update tchc_visualization.R

--- tchc_visualization.R
+++ tchc_visualization.R
@@ -2,6 +2,7 @@ rm(list=ls())
 
 library(magrittr)
 library(leaflet)
+library(htmlwidgets)
 
 setwd("/Users/kevinquach/Dropbox/DataScience/TCHC")
 
@@ -11,7 +12,12 @@ m <- leaflet(df) %>%
   addTiles() %>%  # Add default OpenStreetMap map tiles
   addMarkers(lng=~LONGITUDE,
              lat=~LATITUDE, 
-             popup=paste("<b>Street Address:</b>", df$DEV_ID, df$DEV_NAME),
+             popup=paste("<b>Development Name:</b>", df$DEV_NAME, "<br>",
+                         "<b>Year Built:</b>", df$YEAR_BUILT, "<br>",
+                         "<b>Building Typology:</b>", df$BUILDING, "<br>",
+                         "<b>Total Residential Units:</b>", df$TOTAL_RESU),
              clusterOptions = markerClusterOptions())
              
 m  # Print the map
+
+saveWidget(m, file = "m.html")
